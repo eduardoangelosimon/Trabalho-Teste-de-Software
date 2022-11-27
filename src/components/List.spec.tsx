487999469a992abf2ea9398e45a3548bb32b34c0
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2022-11-27 15:45:29 -0300

    Correção no código

--- src/components/List.spec.tsx
+++ src/components/List.spec.tsx
@@ -3,17 +3,11 @@ import userEvent from '@testing-library/user-event'
 import List from './List' 
 
 describe('List Component', () => {
-  it('should render list items', async () => {
+  it('should render Eduardo and Matheus on the list', async () => {
     const { getByText, queryByText, rerender, unmount } = render(<List initialItems={['Eduardo', 'Matheus']} />)
 
     expect(getByText('Eduardo')).toBeInTheDocument()
     expect(getByText('Matheus')).toBeInTheDocument()
-
-    unmount()
-    rerender(<List initialItems={['Julia']} />)
-
-    expect(getByText('Julia')).toBeInTheDocument()
-    expect(queryByText('Matheus')).not.toBeInTheDocument()
   });
 
   it('should be able to add new item to the list', async () => {
@@ -22,23 +16,24 @@ describe('List Component', () => {
     const inputElement = getByPlaceholderText('Nome do integrante');
     const addButton = getByText('Adicionar');
 
-    userEvent.type(inputElement, 'Novo');
+    userEvent.type(inputElement, 'teste');
     userEvent.click(addButton);
 
     await waitFor(() => {
-      expect(getByText('Novo')).toBeInTheDocument()
+      expect(getByText('teste')).toBeInTheDocument()
     })
   });
 
-  it('should be able to add remove item from the list', async () => {
+  it('should be able to remove all items from the list', async () => {
     const { getAllByText, queryByText } = render(<List initialItems={['Eduardo']} />)
 
-    const removeButtons = getAllByText('Remover');
+    const removeButtons = getAllByText('X');
 
     userEvent.click(removeButtons[0]);
 
     await waitFor(() => {
       expect(queryByText('Eduardo')).not.toBeInTheDocument()
+      expect(queryByText('Matheus')).not.toBeInTheDocument()
     })
   });
 });
